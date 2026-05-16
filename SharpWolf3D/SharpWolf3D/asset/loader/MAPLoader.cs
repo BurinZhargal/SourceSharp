7b74047ad599687dd2a595b6c0e6f18a0752e90d
BJ_Gh <49895341+BJGh@users.noreply.github.com>
2026-05-16 12:16:34 +0300

    Add files via upload
    It was created like its need to be run a Wolfenstein3d on the Independent Source3D of Valve Corp

---
+++ SharpWolf3D/SharpWolf3D/asset/loader/MAPLoader.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using J2N;
+using J2N.IO;
+
+namespace SharpWolf3D.asset.loader
+{
+    public class MAPLoader
+    {
+        public static void Fill<T>(T[] array, int start, int end, T value)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException("array");
+            }
+            if (start < 0 || start >= end)
+            {
+                throw new ArgumentOutOfRangeException("fromIndex");
+            }
+            if (end >= array.Length)
+            {
+                throw new ArgumentOutOfRangeException("toIndex");
+            }
+            for (int i = start; i < end; i++)
+            {
+                array[i] = value;
+            }
+        }
+        private static int[] mapOffsets;
+        private static readonly Dictionary<int, int[][]> MAPS = new Dictionary<int, int[][]>();
+
+        public static string GetEntityMAPS(int code)
+        {
+            // java's get method returns null when the key has no mapping
+            // so we'll do the same
+
+            string val;
+            if (MAPS.TryGetValue(code, out val))
+                return val;
+            else
+                return null;
+        }
+
+        private static readonly Dictionary<int, string> MAP_NAMES = new Dictionary<int, string>();
+
+        public static string GetEntityMAPNAMES(int code)
+        {
+            // java's get method returns null when the key has no mapping
+            // so we'll do the same
+
+            string val;
+            if (MAP_NAMES.TryGetValue(code, out val))
+                return val;
+            else
+                return null;
+        }
+        public static void load(String path, String mapHeadRes, String gameMapsRes)
+        {
+            try
+            {
+               
+                
+                Stream mapHeadIs = new FileStream(path + mapHeadRes, FileMode.Open, FileAccess.Read);
+                Stream gameMapIs = new FileStream(path + gameMapsRes, FileMode.Open, FileAccess.Read);
+                    // extract the header and map offsets info
+                    sbyte[] magic = new sbyte[2];
+                     mapHeadIs.Read((byte[])(Array)magic, 0, magic.Length);
+                //??? ByteBuffer conversion problem
+                    MemoryStream mapHeadData = new MemoryStream();
+                    using(StreamWriter sw = new StreamWriter(mapHeadIs))
+                {
+                    sw.Write(mapHeadIs.ToString());
+                }
+             
+
+                    mapOffsets = new int[100];
+                    int index = 0;
+                    while (index < mapOffsets.Length)
+                    {
+                        mapOffsets[index++] = Convert.ToInt32(mapHeadData);
+                    }
+
+                // extract all the maps
+              
+                ByteBuffer gameMapData = ByteBuffer.Wrap(Convert.FromBase64String(gameMapIs.ToString()));
+                gameMapData.SetOrder(ByteOrder.LittleEndian);
+                    for (int i = 0; i < index; i++)
+                    {
+                        int[][] map = new int[2][];
+                        int mapOffset = mapOffsets[i];
+                        if (mapOffset <= 0)
+                        {
+                            continue;
+                        }
+                        gameMapData.SetPosition(mapOffset);
+                        int offPlane0 = gameMapData.GetInt32();
+                        int offPlane1 = gameMapData.GetInt32();
+                        int offPlane2 = gameMapData.GetInt32();
+                        int lenPlane0 = gameMapData.GetInt16();
+                        int lenPlane1 = gameMapData.GetInt16();
+                        int lenPlane2 = gameMapData.GetInt16();
+                        int width = gameMapData.GetInt16();
+                        int height = gameMapData.GetInt16();
+                        sbyte[] name = new sbyte[16];
+                        gameMapData.Get((byte[])(Array)name);
+                        String mapName = new String(name.ToString());
+
+                        // layer 0
+                        ByteBuffer carmackDecompressed = decompressCarmack(gameMapData, offPlane0);
+
+                        carmackDecompressed.SetOrder(ByteOrder.LittleEndian);
+                        carmackDecompressed.SetPosition(0);
+                        Int32Buffer rlewDecompressed = decompressRLEW(carmackDecompressed, 0);
+                    //?????
+                        map[0] = (int[])(Array)Convert.ToInt32(rlewDecompressed);
+
+                        // layer 1 
+                        carmackDecompressed = decompressCarmack(gameMapData, offPlane1);
+                        carmackDecompressed.SetOrder(ByteOrder.LittleEndian);
+                        carmackDecompressed.SetPosition(0);
+                        rlewDecompressed = decompressRLEW(carmackDecompressed, 0);
+                        map[1] = rlewDecompressed.Array;
+
+                        MAPS[i] = map;
+                        MAP_NAMES[i] = mapName;
+                    }
+                
+            }
+            catch (IOException ex)
+            {
+                throw new Exception("Could not load MAP resources properly!", ex);
+            }
+        }
+        private static ByteBuffer decompressCarmack(ByteBuffer bb, int start) 
+        {
+            byte nearTag = (byte)0xA7;
+            byte farTag = (byte)0xA8;
+            bb.SetPosition(start);
+            byte[] decompressedData = new byte[bb.GetInt16() & 0xFFFF];
+            int decompressedDataIndex = 0;
+            while(decompressedDataIndex< decompressedData.Length)
+            {
+                byte b0 = bb.Get();
+                byte b1 = bb.Get();
+                if((b0==0&&b1==nearTag)||(b0==0&&b1==farTag))
+                {
+                    decompressedData[decompressedDataIndex++] = bb.Get();
+                    decompressedData[decompressedDataIndex++]=b1; 
+                }
+                else if (b1==nearTag||b1==farTag)
+                {
+                    int location = (b1 == nearTag ? decompressedDataIndex - 2 * (bb.Get() & 0xff) : 2 * (bb.GetInt16() & 0xFFFF));
+                    int sizeInBytes = (b0 & 0xff) * 2;
+                    System.Array.Copy(decompressedData, location, decompressedData, decompressedDataIndex, sizeInBytes);
+                    decompressedDataIndex += sizeInBytes;
+                }
+                else
+                {
+                    decompressedData[decompressedDataIndex++] = b0;
+
+                    decompressedData[decompressedDataIndex++] = b1;
+                }
+            }
+            ByteBuffer decompressedByteBuffer = ByteBuffer.Wrap(decompressedData);
+            decompressedByteBuffer.SetOrder(ByteOrder.LittleEndian);
+            return decompressedByteBuffer;
+        }
+        private static Int32Buffer decompressRLEW(ByteBuffer bb, int start)
+        {
+            bb.SetPosition(start);
+            int[] decompressedData = new int[(bb.GetInt16() & 0xFFFF) / 2];
+            int decompressedDataIndex = 0;
+            while(decompressedDataIndex<decompressedData.Length)
+            {
+                short s = bb.GetInt16();
+                if (s == Convert.ToInt16( 0xABCD))
+                {
+                    int count = bb.GetInt16() & 0xffff;
+                    Fill(decompressedData, decompressedDataIndex, decompressedDataIndex + count, bb.GetInt16() & 0xFFFF);
+                    decompressedDataIndex += count;
+                }
+                else
+                {
+                    decompressedData[decompressedDataIndex++] = s & 0xffff;
+                }
+
+            }
+            Int32Buffer decompressedBuffer = Int32Buffer.Wrap(decompressedData);
+            return decompressedBuffer;
+        }
+        public static int[][] getMap (int mapId)
+        {
+            int[][] Map = new int[Convert.ToInt32(GetEntityMAPS(mapId).ToString().Select(o=>Convert.ToInt32(o)).ToArray())][];
+            return Map;
+        }
+        public static String GetMapName (int mapId)
+        {
+            return GetEntityMAPNAMES(mapId);
+        }
+    }
+}
---
+++ SharpWolf3D/SharpWolf3D/asset/loader/VGAGRAPHLoader.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using J2N.IO;
+using System.Drawing.Imaging;
+using J2N.Collections.Generic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Net.NetworkInformation;
+using System.Reflection.Metadata.Ecma335;
+using System.Xml.Linq;
+using System.Runtime.InteropServices;
+
+namespace SharpWolf3D.asset.loader
+{
+    public class VGAGRAPHLoader
+    {
+        public class VGAGRAPHFont
+        {
+            private readonly byte[] fontData;
+            private int fontHeight;
+            private int[] fontLocation;
+            private int[] fontWidth;
+            private readonly Bitmap[] charImages = new Bitmap[256];
+
+            public VGAGRAPHFont(byte[] fontData, Color color)
+            {
+                this.fontData = fontData;
+                extractFontInformation();
+                generateCharImages(color);
+            }
+            private void extractFontInformation()
+            {
+                ByteBuffer bb = ByteBuffer.Wrap(fontData);
+                bb.SetOrder(ByteOrder.LittleEndian);
+                fontHeight = bb.GetInt16() & 0xffff;
+                fontLocation = new int[256];
+                fontWidth = new int[256];
+                for (int i = 0; i < fontLocation.Length; i++)
+                {
+                    fontLocation[i] = bb.GetInt16() & 0xffff;
+                }
+                for (int i = 0; i < fontWidth.Length; i++)
+                {
+                    fontWidth[i] = bb.Get() & 0xff;
+                }
+            }
+            private void generateCharImages(Color color)
+            {
+                for (int c = 0; c < 256; c++)
+                {
+                    int ch = fontHeight;
+                    int cw = fontWidth[c];
+                    int cl = fontLocation[c];
+
+                    Bitmap charImage = null;
+                    if (ch > 0 && cw > 0)
+                    {
+                        charImage = new Bitmap(cw, ch);
+                    }
+                    if (charImage == null) continue;
+                    charImages[c] = charImage;
+                    int index = 0;
+                    for (int y = 0; y < ch; y++)
+                    {
+                        for (int x = 0; x < cw; x++)
+                        {
+                            int rgb = fontData[cl + index++] & 0xff;
+                            if (rgb > 0)
+                            {
+
+                                charImage.SetPixel(x, y, Color.FromArgb(255, 0, 0));
+                            }
+                        }
+                    }
+                }
+            }
+            public void drawString(Graphics g, string text, int x, int y)
+            {
+                int dx = 0;
+                for (int i = 0; i < text.Length; i++)
+                {
+                    int c = text[i];
+                    Bitmap charImage = charImages[c];
+                    g.DrawImage(charImage, x + dx, y);
+                    dx += fontWidth[c];
+                }
+
+            }
+        }
+        private static readonly sbyte[] huffmanNodes = new sbyte[256 * 4];
+        private static FrameDimension[] pictable;
+        private static readonly IDictionary<int, Bitmap> PICS = new Dictionary<int, Bitmap>();
+        private static readonly IDictionary<string, Bitmap> FONTS = new Dictionary<string, Bitmap>();
+
+        public static void load(string path, string vgaHeadRes, string vgaDictRes, string vgaGraphRes)
+        {
+
+            using (var vgaHeadIs = new FileStream(Path.Combine(path, vgaHeadRes), FileMode.Open))
+            using (var vgaDictIs = new FileStream(Path.Combine(path, vgaDictRes), FileMode.Open))
+            using (var vgaGraphIs = new FileStream(Path.Combine(path, vgaGraphRes), FileMode.Open))
+            {//?
+                var vgaHeadData = ByteBuffer.Wrap((byte[])Convert.ToByte(vgaHeadIs.ReadByte()));
+                vgaHeadData.SetOrder(ByteOrder.LittleEndian);
+
+                // size of VGAHEAD file must be multiple of 3
+                if (vgaHeadData.Limit % 3 != 0)
+                {
+                    throw new Exception("VGAHEAD file is corrupted!");
+                }
+
+                // extract the offsets information from the header
+                var picsCount = vgaHeadData.Limit / 3;
+                var offsets = new int[picsCount];
+                for (var i = 0; i < offsets.Length; i++)
+                {
+                    var o0 = vgaHeadData.Get() & 0xff;
+                    var o1 = vgaHeadData.Get() & 0xff;
+                    var o2 = vgaHeadData.Get() & 0xff;
+                    offsets[i] = o0 + (o1 << 8) + (o2 << 16);
+                }
+                ByteBuffer vgaDictData = ByteBuffer.Wrap(vgaDictIs.Read());
+                vgaDictData.SetOrder(ByteOrder.LittleEndian);
+
+                // extract huffman dictionary
+                vgaDictData.Get(huffmanNodes);
+
+                ByteBuffer vgaGraphData
+                        = ByteBuffer.Wrap(vgaGraphIs.Read);
+
+                vgaGraphData.SetOrder(ByteOrder.LittleEndian);
+                for (var i = 0; i < offsets.Length - 1; i++)
+                {
+                    var length = offsets[i + 1] - offsets[i];
+                    var compressed = vgaGraphData.Slice();
+                    compressed.SetOrder(ByteOrder.LittleEndian);
+                    var decompressedData = DecompressHuffman(compressed);
+                    if (decompressedData == null) continue;
+                    // extract pictable
+                    if (i == 0)
+                    {
+                        ExtractPictable(decompressedData, picsCount);
+                    }
+
+                    if (i == 1)
+                    {
+                        // 8x8 small font
+                        var fontWhite = new VGAGRAPHFont(decompressedData, Color.White);
+                        var fontBlack = new VGAGRAPHFont(decompressedData, Color.Black);
+                        var fontYellow = new VGAGRAPHFont(decompressedData, Color.Yellow);
+                        //?
+                        FONTS.Add("SMALL_WHITE", (Bitmap)fontWhite);
+                        FONTS.Add("SMALL_BLACK", (Bitmap)fontBlack);
+                        FONTS.Add("SMALL_YELLOW", (Bitmap)fontYellow);
+                        if (i == 2)
+                        {
+                            // game options font
+                            var fontGray = new VGAGRAPHFont(decompressedData, Color.Gray);
+                            var fontLightGray = new VGAGRAPHFont(decompressedData, Color.LightGray);
+                            var fontDarkRed = new VGAGRAPHFont(decompressedData, Utils.GetColor("0x710000ff"));
+                            var fontYellow = new VGAGRAPHFont(decompressedData, Color.Yellow);
+
+                            FONTS.Add("BIG_GRAY", fontGray);
+                            FONTS.Add("BIG_LIGHT_GRAY", fontLightGray);
+                            FONTS.Add("BIG_DARK_RED", fontDarkRed);
+                            FONTS.Add("BIG_YELLOW", fontYellow);
+                        }
+                        else if (i > 2)
+                        {
+                            Dimension picDimension = pictable[i];
+                            if (picDimension != null)
+                            {
+                                int picWidth = picDimension.width;
+                                int picHeight = picDimension.height;
+                                if (picWidth <= 0 || picHeight <= 0) continue;
+
+                                BufferedImage fixedImage = fixVgaModeYPic(
+                                            i, decompressedData, picWidth, picHeight);
+
+                                PICS.put(i, fixedImage);
+                            }
+                        }
+                    }
+
+                }
+            }
+
+        }
+        private static void ExtractPictable(byte[] pictableData, int picsCount)
+        {
+            int pictableOffsetIndex = 3;
+            pictable = new Dimension[picsCount];
+            for (int i = 0; i < pictableData.Length / 4; i++)
+            {
+                int b0 = pictableData[4 * i + 0] & 0xff;
+                int b1 = pictableData[4 * i + 1] & 0xff;
+                int b2 = pictableData[4 * i + 2] & 0xff;
+                int b3 = pictableData[4 * i + 3] & 0xff;
+                int picWidth = b0 + (b1 << 8);
+                int picHeight = b2 + (b3 << 8);
+                pictable[i + pictableOffsetIndex] = new Dimension(picWidth, picHeight);
+            }
+        }
+
+        private static byte[] DecompressHuffman(ByteBuffer compressed)
+        {
+            int decompressedLength = (int)(compressed.GetInt() & 0xffffffffL);
+            if (decompressedLength <= 0) return null;
+            byte[] data = new byte[decompressedLength];
+            int bitIndex = 0;
+            int dataIndex = 0;
+            int nodeIndex = 254;
+            while (dataIndex < decompressedLength)
+            {
+                int bit = (compressed.Get(compressed.Position) >> bitIndex) & 1;
+                bitIndex++;
+                if (bitIndex > 7)
+                {
+                    bitIndex = 0;
+                    compressed.Position(compressed.Position + 1);
+                }
+                if (compressed.Position >= compressed.Limit) break;
+                if (huffmanNodes[nodeIndex * 4 + 1 + bit * 2] == 0)
+                {
+                    data[dataIndex++] = huffmanNodes[nodeIndex * 4 + bit * 2];
+                    nodeIndex = 254;
+                }
+                else if (huffmanNodes[nodeIndex * 4 + 1 + bit * 2] == 1)
+                {
+                    nodeIndex = huffmanNodes[nodeIndex * 4 + bit * 2] & 0xff;
+                }
+
+            }
+            return data;
+        }
+        public static Bitmap FixVgaModeYPic(int index, byte[] picData, int picWidth, int picHeight)
+        {
+            Bitmap bi = new Bitmap(picWidth, picHeight, PixelFormat.Format8bppIndexed);
+            BitmapData data = bi.LockBits(new Rectangle(0, 0, picWidth, picHeight), ImageLockMode.WriteOnly, bi.PixelFormat);
+            Marshal.Copy(picData, 0, data.Scan0, picData.Length);
+            bi.UnlockBits(data);
+
+            Bitmap fixedImage = new Bitmap(picWidth, picHeight, PixelFormat.Format24bppRgb);
+
+            int w4 = picWidth / 4;
+            int h4 = picHeight / 4;
+            for (int y = 0; y < picHeight; y++)
+            {
+                for (int x = 0; x < picWidth; x++)
+                {
+                    int xs = 4 * (x % w4) + (y / h4);
+                    int ys = 4 * (y % h4) + (x / w4);
+                    Color c = bi.GetPixel(x, y);
+                    fixedImage.SetPixel(xs, ys, c);
+                }
+            }
+
+            return fixedImage;
+        }
+        public static Bitmap GetPic(int picIndex)
+        {
+            return PICS[picIndex];
+        }
+
+        public static VGAGRAPHFont GetFont(string fontId)
+        {
+            return FONTS[fontId];
+        }
+//gotta fix errors
+
+    }
+}
